ef9dd7f96db0981cad0e157edac7930e9530a962
Kirill Zorin <zarincheg@gmail.com>
2024-06-06 00:13:37 +0200

    Fix: NextJS config for standalone building

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	output: 'standalone',
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	serverRuntimeConfig: {
+		maxBodySize: 10 * 1024 * 1024,
+	  },
+};
 
 export default nextConfig;
